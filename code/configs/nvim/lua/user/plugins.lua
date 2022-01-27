872bf1855c5a1c032ee9966598e39b215d675942
Justin Angeles <justinaawd@gmail.com>
2022-01-27 09:43:05 -0800

    auto deploy

--- code/configs/nvim/lua/user/plugins.lua
+++ code/configs/nvim/lua/user/plugins.lua
@@ -73,14 +73,17 @@ return packer.startup(function(use)
   use 'tanvirtin/monokai.nvim'
 
 
+  use "onsails/lspkind-nvim"
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
-  use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
+  use {'tzachar/cmp-tabnine', requires = 'hrsh7th/nvim-cmp'}
+
+
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
 
   -- snippets
