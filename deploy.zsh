86c0c7084f344ba5a50290e4cb5a7150051deeb0
justin angeles <justinaawd@gmail.com>
2022-02-09 06:50:57 -0800

    added safety check to deploy script

--- deploy.zsh
+++ deploy.zsh
@@ -1,7 +1,3 @@
 #!/usr/bin/zsh
 
-sh mkdocs build
-cp ./CNAME ./docs/CNAME 
-git add . 
-git commit -m "$1"
-git push 
+mkdocs build && cp ./CNAME ./docs/CNAME  && git add . && git commit -m "$1" && git push 
